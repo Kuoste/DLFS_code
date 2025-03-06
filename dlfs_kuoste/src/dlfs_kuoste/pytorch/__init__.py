ff8dd2b69cc1fd945bb56919aa54c57785d0de59
Vellu Sorvari <vepeli@gmail.com>
2025-03-06 21:06:00 +0200

    Adding dlfs style pytorch cnn layer. Adding evaluate_networks.py that runs a simple MNIST classification

--- dlfs_kuoste/src/dlfs_kuoste/pytorch/__init__.py
+++ dlfs_kuoste/src/dlfs_kuoste/pytorch/__init__.py
@@ -1,5 +1,5 @@
 from .helpers import permute_data, assert_dim
-from .layers import inference_mode, Layer, DenseLayer
+from .layers import inference_mode, Layer, DenseLayer, ConvLayer
 from .models import Model
 from .preprocessors import ConvNetPreprocessor
 from .trainers import Trainer
